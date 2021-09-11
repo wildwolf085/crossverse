243b9456b6a6a42831d3e401432ca3ae5543dc62
galaxy126 <galaxy85126@gmail.com>
2021-09-11 16:48:22 +0800

    a

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -3,11 +3,11 @@ import classNames from 'classnames'
 import styles from './index.module.scss'
 
 const Block: React.FC<ComponentBase> = ({ className, children }) => {
-  return (
-    <div className={classNames(styles.block, className)}>
-      <div className={classNames(styles['block-inner'])}>{children}</div>
-    </div>
-  )
+	return (
+		<div className={classNames(styles.block, className)}>
+			<div className={classNames(styles['block-inner'])}>{children}</div>
+		</div>
+	)
 }
 
 export default Block
